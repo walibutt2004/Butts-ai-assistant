ac856a01beb7dce88c098b00e0bc24cf9a177e8b
Wali Abdullah <ironmanwaliabdullah@gmail.com>
2026-04-24 14:39:53 +0500

    Image generator added

--- server.js
+++ server.js
@@ -92,7 +92,4 @@ app.post('/generate-image', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server chal raha hai: http://localhost:${PORT}`);
-});const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server chal raha hai: http://localhost:${PORT}`);
 });
